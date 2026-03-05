85defbf4238d4e34809a62f7593b8e7a6949fdeb
Ivan Zakrevsky <ivzak@yandex.ru>
2026-03-05 16:53:47 +0300

    Extract JugglerTaskDepends

--- src/mlx/jira_juggler/jira_juggler.py
+++ src/mlx/jira_juggler/jira_juggler.py
@@ -25,6 +25,7 @@ from mlx.jira_juggler.tasks.properties.allocate import JugglerTaskAllocate
 from mlx.jira_juggler.tasks.properties.base_property import JugglerTaskProperty
 from mlx.jira_juggler.tasks.properties.constants import TODO_STATUSES, PROGRESS_STATUSES, DEVELOPED_STATUSES, \
     RESOLVED_STATUSES, PENDING_STATUSES, DONE_STATUSES, TAB
+from mlx.jira_juggler.tasks.properties.depends import JugglerTaskDepends
 from mlx.jira_juggler.tasks.properties.effort import EmptyPertEstimate, PertEstimate, CompositePertEstimate, \
     JugglerTaskEffort
 from mlx.jira_juggler.tasks.properties.priority import JugglerTaskPriority
@@ -118,77 +119,6 @@ def determine_links(jira_link_types, input_links):
             logging.warning(f"Failed to find links {missing_links} in your configuration in Jira")
         valid_links = unique_input_links - missing_links
     return valid_links
-
-
-class JugglerTaskDepends(JugglerTaskProperty):
-    """Class for linking of a juggler task"""
-
-    DEFAULT_NAME = 'depends'
-    DEFAULT_VALUE = []
-    PREFIX = ''
-    links = set()
-    registry: Registry
-
-    def __init__(self, registry: Registry, jira_issue: jira.Issue | None = None):
-        super().__init__(jira_issue)
-        self.registry = registry
-
-    def append_value(self, value):
-        """Appends value for task juggler property
-
-        Args:
-            value (object): Value to append to the property
-        """
-        if value not in self.value:
-            self.value.append(value)
-
-    def load_from_jira_issue(self, jira_issue):
-        """Loads the object with data from a Jira issue
-
-        Args:
-            jira_issue (jira.resources.Issue): The Jira issue to load from
-        """
-        if hasattr(jira_issue.fields, 'issuelinks'):
-            for link in jira_issue.fields.issuelinks:
-                if hasattr(link, 'inwardIssue') and link.type.inward in self.links:
-                    self.append_value(to_identifier(link.inwardIssue.key))
-                elif hasattr(link, 'outwardIssue') and link.type.outward in self.links:
-                    self.append_value(to_identifier(link.outwardIssue.key))
-
-    def validate(self, task, tasks):
-        """Validates (and corrects) the current task property
-
-        Args:
-            task (JugglerTask): Task to which the property belongs
-            tasks (list): List of JugglerTask instances to which the current task belongs. Will be used to
-                verify relations to other tasks.
-        """
-        """
-        task_ids = [self.registry.path(to_identifier(tsk.key)) for tsk in tasks]
-        for val in list(self.value):
-            if val not in task_ids:
-                logging.warning('Removing link to %s for %s, as not within scope', val, task.key)
-                self.value.remove(val)
-        """
-
-    def __str__(self):
-        """Converts task property object to the task juggler syntax
-
-        Returns:
-            str: String representation of the task property in juggler syntax
-        """
-        if self.value:
-            valstr = ''
-            for val in self.value:
-                val = self.registry.path(val)
-                if valstr:
-                    valstr += ', '
-                valstr += self.VALUE_TEMPLATE.format(prefix=self.PREFIX,
-                                                     value=val,
-                                                     suffix=self.SUFFIX)
-            return self.TEMPLATE.format(prop=self.name,
-                                        value=valstr)
-        return ''
 
 
 class JugglerTaskFlags(JugglerTaskProperty):
