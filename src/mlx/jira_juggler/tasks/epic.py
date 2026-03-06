5747bb344a17ced9c339bfac0d7cbb4268f59220
Ivan Zakrevsky <ivzak@yandex.ru>
2026-03-06 20:28:27 +0300

    Fix Epic time

--- src/mlx/jira_juggler/tasks/epic.py
+++ src/mlx/jira_juggler/tasks/epic.py
@@ -7,6 +7,20 @@ __all__ = ('Epic',)
 
 class Epic(JugglerTask):
 
+    def load_from_jira_issue(self, jira_issue):
+        super().load_from_jira_issue(jira_issue)
+
+        if not self.properties['time'].is_empty and self.properties['time'].name == 'fact:end' and not self.is_complete():
+            # Actually the PBI is not finished yet.
+            self.properties['time'].clear()
+        if not self.properties['time'].is_empty and self.properties['time'].name == 'fact:start' and self.todo():
+            # Actually the PBI is not started yet.
+            self.properties['time'].clear()
+        if not self.properties['time'].is_empty and self.properties['time'].name == 'start' and not self.todo() and self.children:
+            # Started with the first PBI.
+            self.properties['time'].clear()
+
+
     def shift_unstarted_tasks_to_milestone(self, extras, milestone):
         for child in self.children:
             child.shift_unstarted_tasks_to_milestone(extras, milestone)
